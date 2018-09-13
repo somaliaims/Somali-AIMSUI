3d9741321c1e7bfee9c1ad741078ed7be16c7c92
Rashid Ahmad <raashid.ahmad@gmail.com>
2018-09-13 21:09:06 +0500

    Fixed routing issue

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './login/login.component';
 // Route Configuration
 export const routes: Routes = [
   { path: '', component: LoginComponent},
+  { path: 'login', component: LoginComponent },
 ];
 
 export const Routing: ModuleWithProviders = RouterModule.forRoot(routes);
