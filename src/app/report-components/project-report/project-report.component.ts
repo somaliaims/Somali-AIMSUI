19219babe9abe19a60c710b21ac3c60c3bd41a13
Rashid Ahmad <rashid.ahmad@live.com>
2019-06-08 23:38:52 +0500

    Added project profile report

--- src/app/report-components/project-report/project-report.component.ts
+++ src/app/report-components/project-report/project-report.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProjectService } from 'src/app/services/project.service';
+import { BlockUI, NgBlockUI } from 'ng-block-ui';
+import { ErrorModalComponent } from 'src/app/error-modal/error-modal.component';
 
 @Component({
   selector: 'app-project-report',
@@ -12,9 +14,18 @@ export class ProjectReportComponent implements OnInit {
   reportView: string = 'search';
   btnText: string = 'View report';
   isProcessing: boolean = true;
-  model: any = {criteria: null, selectedProjectId: null, projectTitle: null };
-
-  constructor(private projectService: ProjectService) { }
+  projectProfile: any = {
+    locations: [],
+    sectors: [],
+    funders: [],
+    implementers: [],
+    documents: [],
+    customFields: []
+  };
+  model: any = {criteria: null, selectedProjectId: 0, projectTitle: null };
+  @BlockUI() blockUI: NgBlockUI;
+  
+  constructor(private projectService: ProjectService, private errorModal: ErrorModalComponent) { }
 
   ngOnInit() {
     this.getProjects();
@@ -33,13 +44,46 @@ export class ProjectReportComponent implements OnInit {
   }
 
   filterProjects() {
-    var criteria = this.model.criteria;
+    var criteria = this.model.projectTitle;
     if (!criteria) {
       this.filteredProjects = this.projectsList;
     } else {
       criteria = criteria.toLowerCase();
       this.filteredProjects = this.projectsList.filter(p => p.title.toLowerCase().indexOf(criteria) != -1);
     }
+  }
+
+  selectProject(e) {
+    var id = e.currentTarget.id.split('-')[1];
+    if (id) {
+      var selectedProject = this.projectsList.filter(p => p.id == id);
+      if (selectedProject.length > 0) {
+        this.model.selectedProjectId = id;
+        this.model.projectTitle = selectedProject[0].title; 
+      }
+    }
+  }
+
+  viewProjectProfileReport() {
+    if (this.model.selectedProjectId) {
+      this.blockUI.start('Loading report');
+      this.reportView = 'report';
+      this.projectService.getProjectProfileReport(this.model.selectedProjectId).subscribe(
+        data => {
+          if (data) {
+            if (data.projectProfile) {
+              this.projectProfile = data.projectProfile;
+            }
+          }
+          this.blockUI.stop();
+        }
+      );
+    } 
+  }
+
+  reset() {
+    this.model.selectedProjectId = 0;
+    this.model.projectTitle = null;
   }
 
 }
