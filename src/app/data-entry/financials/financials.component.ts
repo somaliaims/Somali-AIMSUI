cc6fb45a4e53d03425f9868b93d2126b3c0b035b
Rashid Ahmad <rashid.ahmad@live.com>
2019-09-06 00:17:36 +0500

    Added financials tab to data entry, basic ui work

--- src/app/data-entry/financials/financials.component.ts
+++ src/app/data-entry/financials/financials.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-financials',
+  selector: 'financials',
   templateUrl: './financials.component.html',
   styleUrls: ['./financials.component.css']
 })
 export class FinancialsComponent implements OnInit {
 
+  @Input()
+  currenciesList: any = [];
+  @Input()
+  projectCurrency: string = null;
+  @Input()
+  projectValue: number = 0;
+  @Input()
+  startingYear: number = 0;
+  @Input()
+  endingYear: number = 0;
+
+  currentYear: any = 0;
+  yearList: any = [];
+
+  disbursementModel: any = { currency: null, projectValue: 0 };
   constructor() { }
 
   ngOnInit() {
+    this.disbursementModel.currency = this.projectCurrency;
+    this.disbursementModel.projectValue = this.projectValue;
+    this.setYearsList();
+  }
+
+  setYearsList() {
+    for(var i = this.startingYear; i < this.endingYear; i++) {
+      this.yearList.push(i);
+    }
   }
 
 }
