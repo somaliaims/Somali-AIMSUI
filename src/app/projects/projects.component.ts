125e8a3c638f348a9ea60991e35393356f1c5d6b
Rashid Ahmad <rashid.ahmad@live.com>
2020-12-19 21:52:36 +0500

    Tweaks for ex rates, and projects

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -649,7 +649,6 @@ export class ProjectsComponent implements OnInit {
 
   setReportRenderStatus(status: boolean) {
     this.renderReport = status;
-    console.log('Report render: ' + status);
   }
 
   resetFilters() {
